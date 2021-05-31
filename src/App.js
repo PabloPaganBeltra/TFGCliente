95905227e56e746b6dec0a595d1a936136d18f08
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 22:36:00 +0200

    comit de httprequest

--- src/App.js
+++ src/App.js
@@ -15,7 +15,21 @@ import RutaPrivada from './components/rutas/RutaPrivada';
 const token = localStorage.getItem('token');
 if(token){
   tokenAuth(token);
-}
+};
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+        console.log(xhttp.responseText)
+
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("demo").innerHTML = xhttp.responseText;
+    } else {
+      console.log("error")
+    }
+};
+xhttp.open("GET", "thingproxy.freeboard.io/https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJryijc9s0K4gRG9aU7SDTXdA&key=[YOURAPIKEY]", true);
+xhttp.send();
 
 function App() {
 
